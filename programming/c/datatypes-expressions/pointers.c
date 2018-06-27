31f06c96f91d62ddc939f8048b794eb0b6f61da5
hemmpd <heidi.rytkonen@outlook.com>
2018-06-27 18:07:44 +0300

    done pointer exercise

--- programming/c/datatypes-expressions/pointers.c
+++ programming/c/datatypes-expressions/pointers.c
@@ -4,10 +4,23 @@ int main(int argc, char *argv[])
 {
     int a, b, c;
     // declare a pointer variable and assign it to address of e.g. a
+	int *p;
 
     // Evaluate expressions using both the original and the pointer variable
     // and investigate the value / value pointed to
+	a = 10;
+	b = 4;
+	c = 2;
 
+	p = &a;
+	c += (*p) + b;
+
+	printf("c = %d, should be 16\n", c);
+
+	p = &c;
+	b += 2*(*p);
+
+	printf("b = %d, should be 36\n", b);
 
     return 0;
 }
