aa962c148336947c570ef8b822b842a48ac0962d
hemmpd <heidi.rytkonen@outlook.com>
2018-06-27 11:56:38 +0300

    did the if else exercise

--- programming/c/control-structures/if_else.c
+++ programming/c/control-structures/if_else.c
@@ -2,10 +2,19 @@
 
 int main(int argc, char *argv[])
 {
-    int i;
+    int i = 54;
 
     // write the control structure as described in the exercise
     // and run the code with different values for i
+    if (i < 0) {
+        printf("Integer is negative\n");
+    } else if (i == 0) {
+        printf("Integer is zero\n");
+    } else if (i > 100) {
+	printf("Integer is greater than 100\n");
+    } else {
+	printf("Something else\n");
+    }
 
     return 0;
 }
