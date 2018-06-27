8a97d09bdd3537a41e6128942f63d243f17c4410
hemmpd <heidi.rytkonen@outlook.com>
2018-06-27 11:58:38 +0300

    did the fibonacci exercise

--- programming/c/control-structures/fibonacci.c
+++ programming/c/control-structures/fibonacci.c
@@ -6,6 +6,18 @@ int main(int argc, char *argv[])
 
     // initialize variables and write the while loop
     // Remember to update all variables within the loop
+	f0 = 0;
+	f1 = 1;
+
+	printf("\t %d \n", f0);
+	printf("\t %d \n", f1);
+
+	while (f0 < 100) {
+		f0 = f1 + f2;
+		f2 = f1;
+		f1 = f0;
+		printf("\t %d \n", f0);
+	}
 
     return 0;
 }
