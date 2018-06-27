581d7ebe59b79c6533627837e04e260d06b5f092
hemmpd <heidi.rytkonen@outlook.com>
2018-06-27 11:57:54 +0300

    did the calculate powers exercise

--- programming/c/control-structures/my_pow_prog.c
+++ programming/c/control-structures/my_pow_prog.c
@@ -1,6 +1,7 @@
 /* my_pow.c -- Print out powers of k: 1, k, k^2, k^3, .. up to k^N */
 
 #include <stdio.h>
+#include "math.h"
 
 int main(void)
 {
@@ -15,7 +16,11 @@ int main(void)
 
     /* Add here a for loop that at each iteration prints out
        the current value of the power and updates val */
-#error Add the missing loop
+	for (n = 0; n <= N; n++) {
+		val = pow(k, n);
+		printf("\t %d ", n);
+		printf("\t %d \n", val);
+	}
 
     return 0;
 }
